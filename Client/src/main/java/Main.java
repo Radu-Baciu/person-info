50a9428e46db36365f04707f9c1453ebc0d3ece8
Radu-Baciu <81168417+Radu-Baciu@users.noreply.github.com>
2021-03-22 17:05:16 +0200

    Implemented server functionality to return client name, age and gender, based on client npc

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,2 +1,24 @@
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import proto.PersonInformationGrpc;
+import proto.PersonInfo;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String personName = scanner.next();
+        String personCNP = scanner.next();
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8999).usePlaintext().build();
+
+        PersonInformationGrpc.PersonInformationBlockingStub bookStub = PersonInformationGrpc.newBlockingStub(channel);
+
+        PersonInfo.InfoReply reply = bookStub.giveInfo(PersonInfo.PersonRequest.newBuilder().setName(personName).setCnp(personCNP).build());
+
+        channel.shutdown();
+        scanner.close();
+    }
 }
--- Server/src/main/java/services/PersonInformationImpl.java
+++ Server/src/main/java/services/PersonInformationImpl.java
@@ -4,11 +4,49 @@ import io.grpc.stub.StreamObserver;
 import proto.PersonInfo;
 import proto.PersonInformationGrpc;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class PersonInformationImpl extends PersonInformationGrpc.PersonInformationImplBase{
     @Override
     public void giveInfo(PersonInfo.PersonRequest request, StreamObserver<PersonInfo.InfoReply> responseObserver) {
-        PersonInfo.InfoReply reply = PersonInfo.InfoReply.newBuilder().setMessage("Info: "+request.getName()+ " , "+request.getCnp()).build();
+        char sex = 'U';
+        int year = 0,month = 0,day = 0;
+
+        if(request.getCnp().charAt(0) == '1')
+        {
+            year = 1900 + (request.getCnp().charAt(1) - '0') * 10 + (request.getCnp().charAt(2) - '0');
+            month = (request.getCnp().charAt(3) - '0') * 10 + (request.getCnp().charAt(4) - '0');
+            day = (request.getCnp().charAt(5) - '0') * 10 + (request.getCnp().charAt(6) - '0');
+            sex = 'M';
+        }
+        if(request.getCnp().charAt(0) == '2') {
+            year = 1900 + (request.getCnp().charAt(1) - '0') * 10 + (request.getCnp().charAt(2) - '0');
+            month = (request.getCnp().charAt(3) - '0') * 10 + (request.getCnp().charAt(4) - '0');
+            day = (request.getCnp().charAt(5) - '0') * 10 + (request.getCnp().charAt(6) - '0');
+            sex = 'F';
+        }
+        if(request.getCnp().charAt(0) == '5') {
+            year = 2000 + (request.getCnp().charAt(1) - '0') * 10 + (request.getCnp().charAt(2) - '0');
+            month = (request.getCnp().charAt(3) - '0') * 10 + (request.getCnp().charAt(4) - '0');
+            day = (request.getCnp().charAt(5) - '0') * 10 + (request.getCnp().charAt(6) - '0');
+            sex = 'M';
+        }
+        if(request.getCnp().charAt(0) == '6') {
+            year = 2000 + (request.getCnp().charAt(1) - '0') * 10 + (request.getCnp().charAt(2) - '0');
+            month = (request.getCnp().charAt(3) - '0') * 10 + (request.getCnp().charAt(4) - '0');
+            day = (request.getCnp().charAt(5) - '0') * 10 + (request.getCnp().charAt(6) - '0');
+            sex = 'F';
+        }
+
+        LocalDate l = LocalDate.of(year,month,day);
+        LocalDate now = LocalDate.now();
+        Period diff = Period.between(l,now);
+        int age = diff.getYears();
+
+        PersonInfo.InfoReply reply = PersonInfo.InfoReply.newBuilder().setMessage("Info: "+request.getName()+ " , "+request.getCnp()+ " , "+sex+ " , "+age).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+        System.out.println(reply.getMessage());
     }
 }
