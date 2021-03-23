e38712eca5fa72f71dabf17d3533d4822602b6f0
Radu-Baciu <81168417+Radu-Baciu@users.noreply.github.com>
2021-03-23 05:57:45 +0200

    Very small change to client Main

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -11,14 +11,14 @@ public class Main {
         String isOpen = "1";
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8999).usePlaintext().build();
 
-        PersonInformationGrpc.PersonInformationBlockingStub bookStub = PersonInformationGrpc.newBlockingStub(channel);
+        PersonInformationGrpc.PersonInformationBlockingStub personStub = PersonInformationGrpc.newBlockingStub(channel);
         while (isOpen.equals("1")) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Insert name:");
             String personName = scanner.next();
             System.out.println("Insert CNP");
             String personCNP = scanner.next();
-            PersonInfo.InfoReply reply = bookStub.giveInfo(PersonInfo.PersonRequest.newBuilder().setName(personName).setCnp(personCNP).build());
+            PersonInfo.InfoReply reply = personStub.giveInfo(PersonInfo.PersonRequest.newBuilder().setName(personName).setCnp(personCNP).build());
             System.out.println("Server replied with " + reply);
             System.out.println("Do you still wish to send info (type 1 to continue)");
             isOpen = scanner.next();
